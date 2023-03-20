7a494b37123846fd9508c5c2af4c2fff9d1caf1c
Kuixi Zhu <quishzhu@hotmail.com>
2023-03-20 15:28:54 -0700

    1

--- DataVisulization/StackedBarplot.R
+++ DataVisulization/StackedBarplot.R
@@ -1,37 +1,46 @@
 
 
-getStackBarplot<-function(input_file,barType,topN,outFileAddress){
+getStackedBarplot<-function(inputFile,filter,threshold=NULL,barType,topRow,topColumn,outputFile){
+
   
+  # getStackBarplot draw a stack barplot.
+  # Each row of the inputFile corresponds to one stacked bar in x-axis 
+  # A stacked bar is consisted of the values of the same row.
+  
+  
+  # :param inputFile: The file address of the input file. This file is a matrix with column and row names, 
+  #                   where  each row corresponds to one target and each column corresponds to one source.
+  #                   The value of each cell should be P.value.
+  #                   It is expected that the rows of input file has been already sorted.
+  # :param filter: it is a boolean value. If it is True, Only cells with value less than threshold would be represented in data visulization.
+  # :param threshold: it is a numeric value. Only cells with value less than threshold would be represented in data visulization.
+  # :param barType: It could be 'one' or '-logP'. If it is 'one', the bar length is 1. If it is '-logP', the bar length is -log(P.value)
+  # :param topRow: it is a numeric value. Only visulize the first topRow rows of the inputFile.
+  # :param topRow: it is a numeric value. Only visulize the first topColumn columns of the inputFile.
+  # :param outputFile: The file address of output figure.
+  
+
+
   library(reshape2)
-  library(ggplot2)
+  library(ggplot2)  
+
+  # Read in data
+  dat=read.table(inputFile,sep="\t",header=T,check.names=F,row.names=1)
   
-  # input_file is a matrix 
-  # rows of matrix: targets
-  # columns of the matrix: sources
-  # value of each cell: p-value
-  
-  dat=read.table(sort_file[i],sep="\t",header=T,check.names=F)
-  
-  # Here we only consider cells with significant P.value
-  # the length of bar is 1
-  if(barType == "one"){
-    ind_Significant = dat<0.05
-    ind_inSignificant = dat>=0.05
-    dat[ind_Significant] = 1
+  # filter data and do the numeric transformation
+  if(filter==TRUE){
+      ind_Significant = dat<threshold
+      ind_inSignificant = dat>=threshold    
     dat[ind_inSignificant] = 0
+    if(barType == "one"){  
+      dat[ind_Significant] = 1
+    }  
+    if(barType == "-logP"){   
+      dat[ind_Significant] = -log(dat[ind_Significant])
+    }
   }
-  
-  # Here we only consider cells with significant p-value
-  # the length of bar is -log(P.value)
-  if(barType == "-logP"){
-    ind_Significant = dat<0.05
-    ind_inSignificant = dat>=0.05
-    dat[ind_Significant] = -log(dat[ind_Significant])
-    dat[ind_inSignificant] = 0
-  }
-  
-  #dat=dat[1:topN,]
-  dat=dat[1:topN,1:10]
+
+  dat=dat[1:topRow,1:topColumn]
   sourceName = colnames(dat)
   dat=t(dat)
   dat=data.frame(dat)
@@ -42,8 +51,6 @@ getStackBarplot<-function(input_file,barType,topN,outFileAddress){
   dat_melted$source=as.factor(dat_melted$source)
   dat_melted$value=as.numeric(dat_melted$value)
   
-  base_size=12
-  base_family=""
 
   # Optional Customize color scheme
   #cols<- c("ATP1B1"="magenta","DCAF12"="magenta3", "FIBP"="green", "JMJD6"="green3",
@@ -51,6 +58,8 @@ getStackBarplot<-function(input_file,barType,topN,outFileAddress){
   #         "YWHAZ"="brown")
 
   # draw the stack barplot
+  base_size=12
+  base_family=""
   ggplot(dat_melted, aes(x=variable, y=value, fill=source)) + scale_y_continuous(limits = c(0, 10))+
     geom_bar(stat="identity") + xlab("\nPathway") + ylab("Frequency\n") +
     #scale_fill_manual(values = cols)+    
@@ -58,7 +67,8 @@ getStackBarplot<-function(input_file,barType,topN,outFileAddress){
     theme(axis.text.x = element_text(vjust=0.5),axis.text = element_text(size = rel(0.8),angle=90,hjust=1), axis.ticks = element_line(colour = "black"), 
           legend.key = element_rect(colour = "grey80"), panel.background = element_rect(fill = "white", colour = NA), panel.border = element_rect(fill = NA,colour = "grey50"),
           panel.grid.major = element_line(colour = "grey90",    size = 0.2), panel.grid.minor = element_line(colour = "grey98", size = 0.5), strip.background = element_rect(fill = "grey80",  colour = "grey50", size = 0.2))
-  ggsave(filename=outFileAddress,width=15,height=10)
+  # output the figure        
+  ggsave(filename=outputFile,width=15,height=10)
     
 }
 
---
+++ DataVisulization/example_StackedBarplot.R
@@ -0,0 +1,6 @@
+
+source("D:/JooGitRepo/CommonTool/DataVisulization/StackedBarplot.R")
+getStackedBarplot("D:/JooGitRepo/CommonTool/DataVisulization/input/KDA_sorted_p62.txt",FALSE,NULL,NULL,50,3,"D:/JooGitRepo/CommonTool/DataVisulization/output/Barplot_KDA_sorted_p62.png")
+getStackedBarplot("D:/JooGitRepo/CommonTool/DataVisulization/input/KDA_sorted_pS65.txt",FALSE,NULL,NULL,50,3,"D:/JooGitRepo/CommonTool/DataVisulization/output/Barplot_KDA_sorted_pS65.png")
+getStackedBarplot("D:/JooGitRepo/CommonTool/DataVisulization/input/KDA_sorted_LBD_PRotein_p62_pS65_combine.txt",FALSE,NULL,NULL,50,5,"D:/JooGitRepo/CommonTool/DataVisulization/output/Barplot_KDA_sorted_LBD_PRotein_p62_pS65_combine.png")
+
